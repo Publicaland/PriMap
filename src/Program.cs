a6142c83c8f2e04fafb7add5ab1b19bccc5cfb49
Publicaland <it@rogis.ro>
2026-08-30 16:30:44 +0300

    Add MudBlazor services and authorization policies

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 using PriMap.Components;
 using PriMap.Components.Account;
 using PriMap.Data;
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityRedirectManager>();
@@ -21,9 +24,15 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy(AppPolicies.CanView, p => p.RequireRole(AppRoles.Viewer, AppRoles.Editor, AppRoles.Analyst, AppRoles.Admin))
+    .AddPolicy(AppPolicies.CanEdit, p => p.RequireRole(AppRoles.Editor, AppRoles.Analyst, AppRoles.Admin))
+    .AddPolicy(AppPolicies.CanAnalyze, p => p.RequireRole(AppRoles.Analyst, AppRoles.Admin))
+    .AddPolicy(AppPolicies.CanManage, p => p.RequireRole(AppRoles.Admin));
+
 var connectionString = builder.Configuration.GetConnectionString("SqlConnection") ?? throw new InvalidOperationException("Connection string 'SqlConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sql => sql.UseNetTopologySuite()));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
@@ -31,6 +40,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options =>
         options.SignIn.RequireConfirmedAccount = true;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
     })
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
@@ -38,6 +48,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options =>
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await RoleSeeder.SeedAsync(scope.ServiceProvider);
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -50,6 +65,9 @@ else
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAntiforgery();
 
